cd18066dc020cc4fe9386910a5d02001c6779eef
Seçgin ŞANLI <secgin@Secgin-MacBook-Pro.local>
2021-03-06 14:35:05 +0300

    .

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ window.addEventListener('load', async () => {
     console.log('Service Worker => ', sW);
 
     subscribeButton.addEventListener('click', async () => {
-        console.log('burda');
         const serviceWorker = await navigator.serviceWorker.ready;
         const clientId = await serviceWorker.pushManager.subscribe({
             userVisibleOnly: true,
             applicationServerKey: 'BBvtgfsznUadqpaIj3Yo4rWnzUgS8CWiy30BRokLiKewTRA_0i3DizTGgbv02O9BXZFAMm2o1mGpfJVeM9Pk1Zo'
         });
-        console.log('-----');
         console.log(clientId);
         console.log(JSON.stringify(clientId));
-        console.log(serviceWorker);
     });
 });
