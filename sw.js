5a6a2b134c8c3452441f4067f1c6e70bb1c176cf
Seçgin ŞANLI <secgin@Secgin-MacBook-Pro.local>
2021-03-06 15:00:03 +0300

    Update

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
 self.addEventListener('push', (e) => {
     const config = {
-        body: 'Mesaj içeriği burada yer alacaktır...',
+        body: e.data.text() | 'Yeni Makale',
         data: {
             dateOfArrival: Date.now(),
             primaryKey: '1'
-        }
+        },
+        icon: 'images/logo.jpeg',
+        vibrate: [100, 50, 100]
     };
 
     e.waitUntil(
-        self.registration.showNotification("Yeni Makale Eklendi", config));
+        self.registration.showNotification('Makale başlık', config));
 });
